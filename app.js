e833be7ae9a4ccd83c58f4d5d5a5950c7874eed9
Michael <micha_93_93@mail.ru>
2022-10-26 13:11:11 +0400

    Implemented the recommendations

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const headerButtonPrev = document.querySelector(".left")
-const headerButtonNext = document.querySelector(".right")
+const headerButtonPrev = document.querySelector(".left");
+const headerButtonNext = document.querySelector(".right");
+const listBanners = document.querySelectorAll("#banner-top > div");
+let index = 0;
 
-const listBanners = document.querySelectorAll("#banner-top > div")
-let index = 0
+headerButtonNext.addEventListener('click', () => {
+    index = (index + 1) % listBanners.length;
+    updateSelection();
+});
 
-headerButtonNext.addEventListener('click', function() {
-    index = (index + 1) % listBanners.length
-    updateSelection()
-  })
-
-headerButtonPrev.addEventListener('click', function() {
-    index = (index + listBanners.length - 1) % listBanners.length 
-    updateSelection()
-  })  
+headerButtonPrev.addEventListener('click', () => {
+    index = (index + listBanners.length - 1) % listBanners.length; 
+    updateSelection();
+});  
 
 function updateSelection() {
-    let active = document.querySelector('#banner-top > div.active') 
-    
-    if( active ) active.classList.remove('active')
-    
-    listBanners[index].classList.add('active')
-  }
-
+    let active = document.querySelector('#banner-top > div.active');
+    if(active) {
+      active.classList.remove('active');
+      listBanners[index].classList.add('active');
+    };
+};
