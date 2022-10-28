66627afa1c99774e8f3329510e1112601bb4e805
Michael <micha_93_93@mail.ru>
2022-10-28 10:52:25 +0400

    Made the general contractor section code universal

--- app.js
+++ app.js
@@ -20,3 +20,32 @@ function updateSelection() {
       listBanners[index].classList.add('active');
     };
 };
+
+function activationContent(classList) {
+  const listContent = document.querySelectorAll(".video-contant > div"); 
+  for(let key in listContent){
+    if(listContent.hasOwnProperty(key)){ 
+      if(listContent[key].classList[0] === classList) {     
+        document.querySelector('.video-contant > .active').classList.remove('active');
+        listContent[key].classList.add('active');        
+      };
+    };
+  };
+}; 
+
+function activationButton(classList) { 
+  const listButton = document.querySelectorAll(".xz > div > .butt");
+  for(let key in listButton){
+    if(listButton.hasOwnProperty(key)){
+      if(listButton[key].classList[0] === classList) {
+        document.querySelector('.xz > div > .active').classList.remove('active');
+        listButton[key].classList.add('active');
+      }; 
+    };
+  };
+};
+
+document.querySelector(".xz").addEventListener('click', (f) => {
+    activationButton(f.target.classList[0]);
+    activationContent(f.target.classList[0]);
+  });
