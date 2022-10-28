325e0542130a996634f31206390269c4d389c522
Michael <micha_93_93@mail.ru>
2022-10-28 16:14:56 +0400

    Searched by ID

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ function updateSelection() {
     };
 };
 
-function activationContent(classList) {
+function activationContent(contid) {
   const listContent = document.querySelectorAll(".video-contant > div"); 
-  for(let key in listContent){
-    if(listContent.hasOwnProperty(key)){ 
-      if(listContent[key].classList[0] === classList) {     
+  for(let key in listContent) {
+    if(listContent.hasOwnProperty(key)) { 
+      if(listContent[key].dataset.contid === contid) {     
         document.querySelector('.video-contant > .active').classList.remove('active');
         listContent[key].classList.add('active');        
       };
@@ -33,11 +33,11 @@ function activationContent(classList) {
   };
 }; 
 
-function activationButton(classList) { 
+function activationButton(butid) { 
   const listButton = document.querySelectorAll(".xz > div > .butt");
-  for(let key in listButton){
-    if(listButton.hasOwnProperty(key)){
-      if(listButton[key].classList[0] === classList) {
+  for(let key in listButton) {
+    if(listButton.hasOwnProperty(key)) { 
+      if(listButton[key].dataset.butid === butid) {
         document.querySelector('.xz > div > .active').classList.remove('active');
         listButton[key].classList.add('active');
       }; 
@@ -46,6 +46,16 @@ function activationButton(classList) {
 };
 
 document.querySelector(".xz").addEventListener('click', (f) => {
-    activationButton(f.target.classList[0]);
-    activationContent(f.target.classList[0]);
-  });
+  function searchId (f) {
+    let listPath = f;
+    for(let key in listPath) {
+      if(listPath[key].dataset) {
+        if(listPath[key].dataset.butid) {
+          activationButton(listPath[key].dataset.butid);
+          activationContent(listPath[key].dataset.butid);
+        };
+      };  
+    };
+  };
+searchId(f.path);
+});
