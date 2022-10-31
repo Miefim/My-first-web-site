88f4e844624890e1a703fa33f50adf29d928ca3f
Michael <micha_93_93@mail.ru>
2022-10-31 14:51:56 +0400

    Advanced contract block code

--- app.js
+++ app.js
@@ -21,41 +21,14 @@ function updateSelection() {
     };
 };
 
-function activationContent(contid) {
-  const listContent = document.querySelectorAll(".video-contant > div"); 
-  for(let key in listContent) {
-    if(listContent.hasOwnProperty(key)) { 
-      if(listContent[key].dataset.contid === contid) {     
-        document.querySelector('.video-contant > .active').classList.remove('active');
-        listContent[key].classList.add('active');        
-      };
+const contractButtonArray = document.querySelectorAll('.xz > div > .butt');
+contractButtonArray.forEach((button) => button.addEventListener('click', (e) => {
+  const idContract = e.currentTarget.dataset.butid;
+  const nowActiveContentBlock = document.querySelector(".video-contant > .active")
+    if(nowActiveContentBlock.dataset.contid !== idContract) {
+      nowActiveContentBlock.classList.remove('active');
+      document.querySelector('.xz > div > .active').classList.remove('active');
+      document.querySelector(`[data-butid="${idContract}"]`).classList.add('active');
+      document.querySelector(`[data-contid="${idContract}"]`).classList.add('active');
     };
-  };
-}; 
-
-function activationButton(butid) { 
-  const listButton = document.querySelectorAll(".xz > div > .butt");
-  for(let key in listButton) {
-    if(listButton.hasOwnProperty(key)) { 
-      if(listButton[key].dataset.butid === butid) {
-        document.querySelector('.xz > div > .active').classList.remove('active');
-        listButton[key].classList.add('active');
-      }; 
-    };
-  };
-};
-
-document.querySelector(".xz").addEventListener('click', (f) => {
-  function searchId (f) {
-    let listPath = f;
-    for(let key in listPath) {
-      if(listPath[key].dataset) {
-        if(listPath[key].dataset.butid) {
-          activationButton(listPath[key].dataset.butid);
-          activationContent(listPath[key].dataset.butid);
-        };
-      };  
-    };
-  };
-searchId(f.path);
-});
+}));
